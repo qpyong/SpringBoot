2b4b61f6077e9949cbc891d9a6607d89d2f1d54f
qpyong <qpy_2006@163.com>
2019-02-14 15:04:41 +0800

    整合redis

--- springboot-demo/src/main/java/org/qpyong/demos/springboot/controller/jpa/UserController.java
+++ springboot-demo/src/main/java/org/qpyong/demos/springboot/controller/jpa/UserController.java
@@ -1,11 +1,16 @@
 package org.qpyong.demos.springboot.controller.jpa;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.qpyong.demos.springboot.domain.User;
 import org.qpyong.demos.springboot.domain.UserRepository;
+import org.qpyong.demos.springboot.service.redis.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import sun.org.mozilla.javascript.internal.json.JsonParser;
 
 @Controller
 @RequestMapping(path = "/user")
@@ -14,10 +19,13 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RedisService redisService;
+
+
     /**
-     * 请求参数版本的添加用户。并返回视图名(模板页面：freemarker.ftl/thymeleaf.html）
+     * 请求参数版本的添加用户。并返回视图名(模板页面：freemarker.html/thymeleaf.html）
      * <p>使用freemarker模板，必须设置spring.freemarker.request-context-attribute=request
-     * <p>使用thymeleaf模板，
      *
      * @return
      */
@@ -29,7 +37,18 @@ public class UserController {
         user.setPassword(password);
         userRepository.save(user);
         model.addAttribute("user", user);
+        redisService.put("users", user.getId().toString(), JSONObject.toJSONString(user));
         return "freemarker";
+    }
+
+    @GetMapping(path = "/{userId}")
+    @ResponseBody
+    public User getUser(@PathVariable String userId) {
+        if(redisService.exists("users", userId)) {
+            String userStr = redisService.get("users", userId);
+            return (User)JSONObject.parseObject(userStr,User.class);
+        }
+        return userRepository.findOne(Integer.valueOf(userId));
     }
 
 }
---
+++ springboot-demo/src/main/resources/readme.txt
@@ -0,0 +1,3 @@
+0.0.1-基本框架搭建
+0.0.2-整合mysql、jpa、freemarker
+0.0.3-整合redis
